1e6c3761b87f213df9f7b723a158b120f1158c7c
ncoletto <ncoletto@gmail.com>
2015-06-17 17:18:38 -0300

    loginapp
    version 2014 1

---
+++ validacion_contra_base_datos.php
@@ -0,0 +1,50 @@
+<?php
+$conexion = mysql_connect("localhost", "amphpziw_hermes", "Hermesinteligente5");
+mysql_select_db("amphpziw_hermesinteligente", $conexion);
+/* Extrae los valores enviados desde la aplicacion movil */
+$usuarioEnviado = $_GET['usuario'];
+$passwordEnviado = $_GET['password'];
+
+/* revisar existencia del usuario con la contraseña en la bd */
+$sqlCmd = "SELECT nombreUsuario,password,email FROM usuario WHERE nombreUsuario LIKE '".mysqli_real_escape_string($usuarioEnviado)."' AND password=PASSWORD('".mysqli_real_escape_string($passwordEnviado)."') LIMIT 1";
+$sqlQry = mysqli_query($sqlCmd,$sqlHp);
+if(mysqli_num_rows($sqlQry)>0)
+{
+$login=1;
+}
+else
+{
+$login=0;
+}
+
+/* Define los valores que seran evaluados, en este ejemplo son valores estaticos,
+en una verdadera aplicacion generalmente son dinamicos a partir de una base de datos */
+
+/* Extrae los valores enviados desde la aplicacion movil */
+$usuarioEnviado = $_GET['usuario'];
+$passwordEnviado = $_GET['password'];
+
+/* crea un array con datos arbitrarios que seran enviados de vuelta a la aplicacion */
+$resultados = array();
+$resultados["hora"] = date("F j, Y, g:i a");
+$resultados["generador"] = "Enviado desde metin2imperio.com" ;
+
+/* verifica que el usuario y password concuerden correctamente */
+if( $login==1 ){
+/*esta informacion se envia solo si la validacion es correcta */
+$resultados["mensaje"] = "Validacion Correcta";
+$resultados["validacion"] = "ok";
+
+}else{
+/*esta informacion se envia si la validacion falla */
+$resultados["mensaje"] = "Usuario y password incorrectos";
+$resultados["validacion"] = "error";
+}
+
+/*convierte los resultados a formato json*/
+$resultadosJson = json_encode($resultados);
+
+/*muestra el resultado en un formato que no da problemas de seguridad en browsers */
+echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
+
+?>
